aad64afe19f99f820d6e045908990c0cbdb3e3c3
Vitaliy B <boev182@gmail.com>
2024-01-01 17:50:28 +0500

    3 задача

---
+++ ДЗ/Промежуточная аттестация/3 задача/Program.cs
@@ -0,0 +1,19 @@
+﻿class MainReturn
+{
+static void Main(string[] args)
+    {
+        int[] array = { 1, 2, 5, 0, 10, 34 };
+
+        PrintArrayReversed(array, array.Length - 1);
+    }
+
+    static void PrintArrayReversed(int[] arr, int index)
+    {
+        if (index >= 0)
+        {
+            Console.Write(arr[index] + " " );
+            PrintArrayReversed(arr, index - 1);
+        }
+ 
+    }
+}
